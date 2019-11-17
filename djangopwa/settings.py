8823252e13c1aa5a646b97eaff58a17808f1ba1b
Jorge <jorgealejandro2116@gmail.com>
2019-11-17 12:54:36 -0800

    aa

--- djangopwa/settings.py
+++ djangopwa/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'ol+=w+20%xs++smgh!cn_+4hx%lxi7a1%(p**e)=o#mlzncbfg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
